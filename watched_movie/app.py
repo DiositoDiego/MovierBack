25c660c3c76144c122b21bfbb06b73b9ebc857a6
BetjaderOD <20213tn032@utez.edu.mx>
2024-06-05 13:04:38 -0600

    [ADD] Watched crear y ver lista

--- watched_movie/app.py
+++ watched_movie/app.py
@@ -7,17 +7,29 @@ rds_password = "admin123"
 rds_db = "movier"
 
 def lambda_handler(event, context):
-    body = json.loads(event['body'])
-    user_id = body.get('user_id')
-    movie_id = body.get('movie_id')
-
-    if not user_id or not movie_id:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({'message': 'Faltan user_id o movie_id en el cuerpo de la solicitud'})
-        }
-
     try:
+        body = json.loads(event['body'])
+        user_id = body.get('user_id')
+        movie_id = body.get('movie_id')
+
+        if not user_id or not movie_id:
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'message': 'Faltan user_id o movie_id en el cuerpo de la solicitud'})
+            }
+
+        if not user_exists(user_id):
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'message': 'El usuario no existe'})
+            }
+
+        if not movie_exists(movie_id):
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'message': 'La película no existe'})
+            }
+
         mark_movie_as_watched(user_id, movie_id)
     except Exception as e:
         return {
@@ -30,6 +42,32 @@ def lambda_handler(event, context):
         'body': json.dumps({'message': 'Película marcada como vista con éxito'})
     }
 
+def user_exists(user_id):
+    connection = pymysql.connect(host=rds_host, user=rds_user, password=rds_password, db=rds_db)
+    try:
+        with connection.cursor() as cursor:
+            query = "SELECT 1 FROM Users WHERE id = %s"
+            cursor.execute(query, (user_id,))
+            result = cursor.fetchone()
+            if result:
+                return True
+            return False
+    finally:
+        connection.close()
+
+def movie_exists(movie_id):
+    connection = pymysql.connect(host=rds_host, user=rds_user, password=rds_password, db=rds_db)
+    try:
+        with connection.cursor() as cursor:
+            query = "SELECT 1 FROM Movies WHERE id = %s"
+            cursor.execute(query, (movie_id,))
+            result = cursor.fetchone()
+            if result:
+                return True
+            return False
+    finally:
+        connection.close()
+
 def mark_movie_as_watched(user_id, movie_id):
     connection = pymysql.connect(host=rds_host, user=rds_user, password=rds_password, db=rds_db)
 
@@ -37,10 +75,10 @@ def mark_movie_as_watched(user_id, movie_id):
         with connection.cursor() as cursor:
             query = """
                 INSERT INTO WatchedMovies (user_id, movie_id, status)
-                VALUES (%s, %s, %s)
+                VALUES (%s, %s, 1)
                 ON DUPLICATE KEY UPDATE status = VALUES(status)
             """
-            cursor.execute(query, (user_id, movie_id, 1))
+            cursor.execute(query, (user_id, movie_id))
             connection.commit()
     finally:
         connection.close()
