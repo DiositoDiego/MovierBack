83ffb0dc12f821530ead84e7695538a93c7e0758
BetjaderOD <20213tn032@utez.edu.mx>
2024-06-28 12:47:55 -0600

    [ADD] Test update movie

---
+++ tests/unit/test_app_update_movie.py
@@ -0,0 +1,40 @@
+import json
+import unittest
+from unittest.mock import patch, Mock
+import pymysql
+from movies.update_movie import app
+
+mock_body = {
+    "pathParameters": {
+        "id": "1"
+    },
+    "body": json.dumps({
+        "title": "Test1",
+        "description": "Test1",
+        "genre": "Comedia",
+        "image": "dsad",
+    })
+}
+
+
+class TestLambdaHandler(unittest.TestCase):
+
+    @patch.dict("os.environ", {"REGION_NAME": "us-east-2", "DATA_BASE": "movier-test"})
+    @patch("movies.update_movie.app.title_exists")
+    @patch("movies.update_movie.app.update_movie")
+    @patch("pymysql.connect")
+    def test_lambda_handler(self, mock_connect, mock_update_movie, mock_title_exists):
+        mock_connect.return_value = Mock()
+        mock_title_exists.return_value = False
+        mock_update_movie.return_value = None
+
+        result = app.lambda_handler(mock_body, None)
+
+        self.assertEqual(result['statusCode'], 200)
+        body = json.loads(result['body'])
+        self.assertIn("message", body)
+        self.assertEqual(body["message"], "Película actualizada correctamente")
+
+        mock_title_exists.assert_called_once_with("Test1", "1")
+        mock_update_movie.assert_called_once_with("1", "Test1", "Test1", "Comedia", "dsad", None)
+
